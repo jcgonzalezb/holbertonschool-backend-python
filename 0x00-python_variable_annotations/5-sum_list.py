d639767258e693f81476742af6eccc7242f641e1
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 13:02:28 -0500

    syntax fixed

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-'''
+"""
 This project module contains a type-annotated function that takes a list 
 of floats as argument and returns their sum as a float.
-'''
+"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    '''
+    """
     This is a method that takes a list of floats
     as argument and returns their sum as a float.
     Returns:
         A sum of floats as a float.
-    '''
+    """
     return sum(input_list)
