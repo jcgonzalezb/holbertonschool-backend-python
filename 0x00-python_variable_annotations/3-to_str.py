da0c471362dd6c6a9649e51c4bc6aae7a9947ba0
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 11:12:55 -0500

    initial version

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This project module contains a type-annotated function that takes a float
+argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This is a method that takes a float arguments
+    and returns the floor of the float.
+    Returns:
+        The floor of the float.
+    """
+    return int(n)
