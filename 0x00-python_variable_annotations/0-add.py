a8d124e5e00d0db92dc13536ac645b7f81088aac
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 10:52:55 -0500

    return fixed

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,15 @@
-ls
+#!/usr/bin/env python3
+"""
+This project module contains a type-annotated function that takes two
+float arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This is a method that takes two float arguments
+    and returns their sum as a float.
+    Returns:
+        Sum of two arguments.
+    """
+    return (a + b)
