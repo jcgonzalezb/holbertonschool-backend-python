72b6833ab4a2aa45055709838898ac2edfd3fd4d
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 12:55:30 -0500

    synatx fixed

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -7,11 +7,10 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """
+    '''
     This is a method that takes a list of floats
     as argument and returns their sum as a float.
     Returns:
         A sum of floats as a float.
-    """
-
+    '''
     return sum(input_list)
