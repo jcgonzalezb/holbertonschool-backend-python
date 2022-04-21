5dae6f5c963cd3539148ddcec85093d21f51d1b4
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 11:02:59 -0500

    return fixed

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This project module contains a type-annotated function that takes two
+string arguments and returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This is a method that takes two string arguments
+    and returns a concatenated string.
+    Returns:
+        A concatenated string.
+    """
+    return str1 + str2
