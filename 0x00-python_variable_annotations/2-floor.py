31509253e9e733481a6d38b293ee3f9d6fed8d4e
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 11:32:45 -0500

    Typo fixed

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -7,7 +7,7 @@ argument and returns the floor of the float.
 
 def floor(n: float) -> int:
     """
-    This is a method that takes a float arguments
+    This is a method that takes a float argument
     and returns the floor of the float.
     Returns:
         The floor of the float.
