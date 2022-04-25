844a5f5a99e4f98f8746e4e9836799c4280d9f08
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-25 12:08:21 -0500

    initial version

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This project module contains an asynchronous coroutine that
+takes in two integer arguments and returns a list of random
+delays printed a number of times. The list should be in
+ascending order.
+"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> float:
+    """
+    This is an asynchronous coroutine that takes in two
+    integer arguments (n and max_delay) and returns a list
+    list of all the delays (float values). The number of
+    delays is the same as n. The list should be in ascending order
+    without using sort() because of concurrency.
+    Returns:
+        A list of all the delays (float values).
+    """
+    wait_list = []
+
+    for i in range(0, n):
+        wait_list.append(await wait_random(max_delay))
+        i += 1
+
+    return wait_list
