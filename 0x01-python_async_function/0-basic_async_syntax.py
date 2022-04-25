dea296540cc8811981f0ca887e6ee954e67afaeb
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-25 17:11:22 -0500

    comments fixed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,8 @@
 """
 This project module contains an asynchronous coroutine that
 takes an integer argument with a default value of 10, waits
-for a random delay (float) between 0 and 10 and returns it.
+for a random delay (float) between 0 and 10 and then returns
+the value of the delay.
 """
 
 import random
@@ -12,7 +13,8 @@ import asyncio
 async def wait_random(max_delay: int = 10) -> float:
     """
     This is an asynchronous coroutine that takes a parameter
-    (integer) and returns a random delay (float).
+    (integer), waits for a random delay and returns the value
+    of the delay (float).
     Returns:
         Value of random delay (float).
     """
