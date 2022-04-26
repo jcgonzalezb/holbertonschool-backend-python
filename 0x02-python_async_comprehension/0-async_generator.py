11236ea73045476a3b75d52f8178b60bd0de18f5
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-26 12:21:15 -0500

    yield position changed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -20,5 +20,5 @@ async def async_generator():
         Random number between 0 and 10.
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
