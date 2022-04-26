57851d97aa6c59fc0e30b46d3619a84c7a00d8d8
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-26 13:56:44 -0500

    output fixed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -12,7 +12,7 @@ async_generator = __import__(
     '0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     """
     This is an asynchronous coroutine that takes no
     arguments. The coroutine will collect 10 random
