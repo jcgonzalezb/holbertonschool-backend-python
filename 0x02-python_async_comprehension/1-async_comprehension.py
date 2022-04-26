b7e2b9be17e4a8b6f9915baa192577dffa25131d
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-26 12:12:06 -0500

    PEP8 fixed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,8 @@ async_generator, then return the 10 random numbers.
 """
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = __import__(
+    '0-async_generator').async_generator
 
 
 async def async_comprehension():
