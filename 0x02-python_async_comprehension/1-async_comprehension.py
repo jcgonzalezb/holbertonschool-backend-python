1d1ddc3634706d96d9865af230fe1e8273891142
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-26 11:18:54 -0500

    syntax fixed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """
-This project module contains a coroutine that takes no
-arguments. The coroutine will loop 10 times, each time
-asynchronously wait 1 second, then yield a random 
-number between 0 and 10.
+This project module contains a asynchronous coroutine 
+that takes no arguments. The coroutine will collect
+10 random numbers using an async comprehensing over
+async_generator, then return the 10 random numbers.
 """
 
-import asyncio
-import random
 
 async_generator = __import__('0-async_generator').async_generator
 
@@ -15,10 +13,10 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension():
     """
     This is an asynchronous coroutine that takes in no
-    arguments. The coroutine will loop 10 times, each
-    time asynchronously wait 1 second, then yield a
-    random number between 0 and 10.
+    arguments. The coroutine will collect 10 random numbers
+    using an async comprehensing over async_generator. 
     Returns:
-        Random number between 0 and 10.
+        A list of 10 random numbers.
     """
-    result = [i async for i in asyncio.run(async_generator())]
+    result = [i async for i in async_generator()]
+    return result
