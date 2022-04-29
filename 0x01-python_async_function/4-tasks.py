99b8141c64efc3a17be4aac61a1a04d6875caee7
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-29 10:01:40 -0500

    comments fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-""" Multiple coroutines """
+"""
+This project module contains an asynchronous coroutine that
+takes in two integer arguments and returns a list of random
+delays printed a number of times. The list should be in
+ascending order.
+"""
 
 import asyncio
 from typing import List
@@ -8,7 +13,15 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """returns a list of completed n tasks"""
+    """
+    This is an asynchronous coroutine that takes in two
+    integer arguments (n and max_delay) and returns a list
+    of all the delays (float values). The number of delays
+    is the same as n. The list should be in ascending order
+    without using sort().
+    Returns:
+        A list of all the delays (float values).
+    """
     concurrent_tasks = [task_wait_random(max_delay)
                         for _ in range(n)]
     completed_tasks = []
