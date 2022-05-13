b53dfa4cf815c07cc4b6d10f691c36d39b230f82
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-12 20:41:09 -0500

    initial version

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" This project module contains the first unit test
+for utils.access_nested_map.
+"""
+from parameterized import parameterized
+import unittest
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TestAccessNestedMap inherits from unittest.TestCase to test
+    utils.access_nested_map.
+    """
+    @parameterized.expand([
+        nested_map={"a": 1}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a", "b")
+    ])
+    
+    def TestAccessNestedMap(self, nested_map: Mapping, path: Sequence):
+        """ Method to test that the method returns what it is supposed to.
+        """
+        self.assertEqual(nested_map, path)
+
+
+if __name__ == '__main__':
+    unittest.main()
