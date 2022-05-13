72ffa4ff4f63c12b792ff764f53a91c2dc7df44b
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-13 13:49:48 -0500

    PEP8 fixed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,10 +27,9 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """ Tests if access_nested_map method returns what it is supposed to.
+        """ Tests if access_nested_map method raise an exception.
         """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
-
 
 
 if __name__ == "__main__":
