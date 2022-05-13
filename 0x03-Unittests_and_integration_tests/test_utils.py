875b4b3b29702f40f0dacac049f68854b4c78fa3
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-12 22:08:41 -0500

    PEP8 fixed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,31 +2,25 @@
 """ This project module contains the first unit test
 for utils.access_nested_map.
 """
+from utils import access_nested_map
 from parameterized import parameterized
+
 import unittest
-from typing import (
-    Mapping,
-    Sequence,
-    Any,
-    Dict,
-    Callable,
-)
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap inherits from unittest.TestCase to test
     utils.access_nested_map.
     """
-    @parameterized.expand(
-        nested_map={"a": 1}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a", "b")
-    )
-
-    def TestAccessNestedMap(self, nested_map, path):
-        """ Method to test that the method returns what it is supposed to.
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """ Tests if access_nested_map method returns what it is supposed to.
         """
-        self.assertEqual(nested_map, path)
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
 
 if __name__ == '__main__':
