4857019a0629e4aed797e7689400e7b673623216
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-13 17:36:00 -0500

    function returned

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,12 +4,14 @@ for utils.access_nested_map.
 """
 
 import unittest
-from utils import access_nested_map
+import requests
+from unittest.mock import patch
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ TestAccessNestedMap inherits from unittest.TestCase to test
+    """ TestAccessNestedMap inherits from unittest. TestCase to test
     utils.access_nested_map.
     """
     @parameterized.expand([
@@ -30,6 +32,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Tests if access_nested_map method raise an exception.
         """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestAccessNestedMap inherits from unittest. TestCase to test
+    utils.get_json.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Tests if get_json method returns the expected result.
+        """
+        with patch('requests.get') as mock:
+            mock.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock.assert_called_once()
 
 
 if __name__ == "__main__":
